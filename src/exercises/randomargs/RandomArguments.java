eb751c896e395595ce0c40ae9549a6161ec80253
Scout <scout.martinelli@siriuscom.com>
2017-07-28 15:17:44 -0500

    added more exercises

--- src/exercises/randomargs/RandomArguments.java
+++ src/exercises/randomargs/RandomArguments.java
@@ -1,14 +1,25 @@
 package exercises.randomargs;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RandomArguments {
-	private List<String> array = new ArrayList<String>();
-	
-	RandomArguments(){
-		array = new ArrayList<String>();
+	public static void main(String[] args) {
+		List<String> arguments = new ArrayList<String>();
+		
+		arguments.add("Scout");
+		arguments.add("Chris");
+		arguments.add("Jen");
+		arguments.add("Will");
+		arguments.add("Braden");
+		arguments.add("Cherie");
+		
+		Collections.shuffle(arguments);
+		
+		for(String arg : arguments) {
+			System.out.println(arg);
+		}
+		
 	}
-	
-	
 }
